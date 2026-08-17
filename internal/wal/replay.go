a289d6d3a03d2cb8b6d51655579b27a308dcdaaa
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-18 02:46:38 +0800

    修复：耐久引擎-bug08

--- internal/wal/replay.go
+++ internal/wal/replay.go
@@ -2,7 +2,6 @@ package wal
 
 import (
 	"encoding/binary"
-	"fmt"
 	"io"
 	"os"
 
@@ -178,7 +177,7 @@ func decodeFrame(typ byte, payload []byte) ([]Record, error) {
 	case TypePut, TypeDelete:
 		r, err := decodeOne(typ, payload)
 		if err != nil {
-			return nil, fmt.Errorf("wal decode: %v", err) // BUG08: not %w
+			return nil, err
 		}
 		return []Record{r}, nil
 	case TypeBatch:
